84a150ed8d8c1d3696b2a5ed6972f70d7c4386c3
Desmond Sow <desmondsow@hotmail.com>
2020-05-11 19:55:45 +0800

    Added https redirection upon login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/'; // original: '/home'
 
     /**
      * Define your route model bindings, pattern filters, etc.
