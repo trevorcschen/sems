358bf9b90d6512c0470efc81f7384d3422d614c4
Komaler <maler9622@yahoo.com>
2020-06-11 16:00:38 +0000

    Update 2020_04_25_113010_create_venues_table.php

--- database/migrations/2020_04_25_113010_create_venues_table.php
+++ database/migrations/2020_04_25_113010_create_venues_table.php
@@ -13,6 +13,7 @@ class CreateVenuesTable extends Migration
      */
     public function up()
     {
+        schema::defaultStringLength(191);
         Schema::create('venues', function (Blueprint $table) {
             $table->id();
             $table->string('name');
