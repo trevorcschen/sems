b6bf5a9fc56b5741d7e6752191c7372fe0912e14
trevorcschen <trevor@veecotech.com>
2020-07-06 01:48:20 +0800

    added event seeder

---
+++ database/seeds/EventSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\Event::class, 1)->create()->each(function ($event)
+        {
+            $user = factory(App\User::class)->create();
+            $user->syncRoles('student');
+            $event->users()->sync($user);
+        });
+    }
+}
